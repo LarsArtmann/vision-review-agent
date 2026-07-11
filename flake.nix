c654d6eab3467f8894da956267e09f3cd63de91c
Lars Artmann <git@lars.software>
2026-07-11 09:57:51 +0200

    fix: replace inputs.self with self in flake outputs

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
           };
 
           checks = {
-            format = config.treefmt.build.check inputs.self;
+            format = config.treefmt.build.check self;
             build = config.packages.default;
             test = config.packages.default.overrideAttrs (_: {
               doCheck = true;
